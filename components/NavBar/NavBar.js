4870dc89c755a8c82290d060a7fd38828b018f38
musylic <lalleman.s@gmail.com>
2023-01-11 19:47:16 +0100

    Laatste veranderingen

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import logo from '../../public/images/logo/Flatcode_logo_BW.png';
+import logo from '../../public/images/logo/Flatcode_GB.png';
 
  
 const NavBar = () => {
